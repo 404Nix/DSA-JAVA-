1cc8ae67266fe803b901689984ef002d70e0ff56
Nikhil Kanojia <nikhilkanojia693@gmail.com>
2024-03-07 22:19:24 +0530

    leetcode
    solve a new leetcode problem

--- test.java
+++ test.java
@@ -1,14 +1,22 @@
 public class test {
-    public static void main(String[] args) {
-        int arr[] = {15,20};
-            System.out.println(searchitem(arr, 5));
-    }
-    static int searchitem(int arr[], int x){
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == x) {
-                return i;
+    public boolean isAcronym(String[] words, String s) {
+        if (words.length != s.length()) {
+            return false;
+        }
+        
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].charAt(0) != s.charAt(i)) {
+                return false;
             }
         }
-        return -1;
+        
+        return true;
+    }
+
+    public static void main(String[] args) {
+        test solution = new test();
+        String[] words = {"apple", "banana"};
+        String s = "ab";
+        System.out.println(solution.isAcronym(words, s));  // Output: true
     }
 }
